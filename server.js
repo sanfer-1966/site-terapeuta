59beb663a85283c3d148032e9179c9f4bdcfe79b
Marcelino <mdmarcelinux@gmail.com>
2026-06-24 14:54:52 -0300

    Corrigir caracteres UTF-8 do português

--- server.js
+++ server.js
@@ -41,8 +41,9 @@ function saveDB() {
 }
 
 app.use(cors());
-app.use(express.json());
-app.use(express.static(path.join(__dirname)));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+app.use(express.static(path.join(__dirname), { charset: 'utf-8' }));
 
 app.post('/api/send', (req, res) => {
   const { name, email, phone, message } = req.body;
